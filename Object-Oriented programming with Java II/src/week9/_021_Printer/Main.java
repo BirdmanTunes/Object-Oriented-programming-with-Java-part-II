d21d7ad3cb71ae73511048397747f35fc7fac696
Luci <Luci@DESKTOP-B83E1CK>
2019-03-22 02:33:38 +0200

    Resource test

---
+++ Object-Oriented programming with Java II/src/week9/_021_Printer/Main.java
@@ -0,0 +1,26 @@
+package week9._021_Printer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+        // The file we read
+		
+        File file = new File("test.txt");
+
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            System.out.println(line);
+        }
+
+        reader.close();
+        
+        System.out.println(file.getPath());
+	}
+
+}
