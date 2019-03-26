aa0d2e04d0e11431af170631306ef92682407684
Luci <Luci@DESKTOP-B83E1CK>
2019-03-27 01:14:10 +0200

    Exercise 033 small main fix

--- Object-Oriented programming with Java II/src/week10/_033_Dungeon/Main.java
+++ Object-Oriented programming with Java II/src/week10/_033_Dungeon/Main.java
@@ -3,7 +3,7 @@ package week10._033_Dungeon;
 public class Main {
 
 	public static void main(String[] args) {
-		Dungeon game = new Dungeon(3, 3, 7, 2, true);
+		Dungeon game = new Dungeon(10, 10, 6, 14, true);
 		game.run();
 	}
 }
